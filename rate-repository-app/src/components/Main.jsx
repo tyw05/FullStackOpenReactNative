a247dc93dc27adf2d7f5345cccc0cd41a36449bf
q79843603 <timicq303@hotmail.com.hk>
2023-12-17 21:59:03 +0800

    ex 10.3

---
+++ rate-repository-app/src/components/Main.jsx
@@ -0,0 +1,21 @@
+import Constants from 'expo-constants';
+import RepositoryList from './RepositoryList';
+import { Text, StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <RepositoryList/>
+    </View>
+  );
+};
+
+export default Main;
